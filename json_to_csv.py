225fbb97a17c0e47017df956120a6e0d31d3c875
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2020-11-07 20:00:46 -0600

    All places

---
+++ json_to_csv.py
@@ -0,0 +1,45 @@
+import csv
+import json
+import os
+
+d = "data/raw"
+rd = "result"
+
+# load all to clean
+
+places = {}
+for f in os.listdir(d):
+    print("Reading {}".format(f))
+    with open("{}/{}".format(d, f), "r", encoding='utf8') as rf:
+        text = rf.read()
+        results = json.loads(text)
+        for result in results:
+            id = result.get("place_id")
+            if places.get(id):
+                print("repeated {}".format(id))
+            else:
+                places[id] = result
+
+print("Total places: {}".format(len(places)))
+
+
+with open("result/places.csv", "w", encoding='utf8') as rf:
+    writer = csv.writer(rf)
+    writer.writerow([
+        "Nombre", "Dirección",
+        "Tipos de negocio", "Link en maps", "latitud", "longitud", "Foto", "ID Google Places"])
+    for i, p in places.items():
+        ref = p.get("photos", [{}])[0].get("photo_reference")
+        lat =  p.get("geometry").get("location").get("lat")
+        long =  p.get("geometry").get("location").get("lng")
+        row = [
+            p.get("name"),
+            p.get("formatted_address") or p.get("vicinity"),
+            ",".join(p.get("types")),
+            "https://www.google.com/maps/search/?api=1&query={},{}".format(lat, long),
+            lat,
+            long,
+            "https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference={}key=AIzaSyBJllAe7ICAfNugS45-BpPLizkw-dFFR7s".format(ref) if ref else "",
+            p.get("place_id"),
+        ]
+        writer.writerow(row)
--- main.py
+++ main.py
@@ -5,24 +5,32 @@ import time
 from t import types
 api_key = ''
 client = googlemaps.Client(key=api_key)
-lat= 19.351291
-long= -99.101664
 
-lat = 19.350411
-long = -99.093321
-r = 590
+areas = {
+    "e1": {
+        "lat": 19.351291,
+        "long": -99.101664,
+        "r": 475,
+    },
+    "fm": {
+        "lat": 19.350411,
+        "long": -99.093321,
+        "r": 590,
+    },
+    "sm": {
+        "lat": 19.355149,
+        "long": -99.087444,
+        "r": 170,
+    }
+}
 
-lat = 19.355149
-long = -99.087444
-r = 170
-
-def read_page(lat, long, t, next_page=None, page=0, r=475):
+def read_page(lat, long, t, next_page=None, page=0, r=475, a=""):
     print("Reading page page {} of type {}".format(page, t))
 
     if next_page:
         url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
         # r = requests.get(url,{"key": api_key, "pagetoken": next_page})
-        time.sleep(5)
+        time.sleep(3)
         r = requests.get(url,params={"key": api_key, "pagetoken": next_page})
         places = r.json()
         # print(places)
@@ -37,10 +45,13 @@ def read_page(lat, long, t, next_page=None, page=0, r=475):
             location="{},{}".format(lat, long),
             radius=r
         )
-    with open("data/raw/{}_{}.json".format(t, page), mode="w") as f:
-        f.write(json.dumps(places.get('results')))
+    if places.get('results'):
+        with open("data/raw/{}_{}_{}.json".format(a, t, page), mode="w") as f:
+            f.write(json.dumps(places.get('results')))
     if places.get('next_page_token'):
-        read_page(lat, long, t, places.get('next_page_token'), page+1, r)
+        read_page(lat, long, t, places.get('next_page_token'), page+1, r, a)
 
-for t in types:
-    read_page(lat, long, t)
+for a, d in areas.items():
+    print("Working with area: {}".format(a))
+    for t in types:
+        read_page(d["lat"], d["long"], t, r=d["r"], a=a)
